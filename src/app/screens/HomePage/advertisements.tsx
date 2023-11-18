02b45dc8415f0fc3b60a70ff9a534f0272f0dd36
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2023-11-18 11:46:34 +0900

    feat: Develop Advertisement component

--- src/app/screens/HomePage/advertisements.tsx
+++ src/app/screens/HomePage/advertisements.tsx
@@ -1,10 +1,24 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 
 export function Advertisements() {
     return (
-        <div className="top_restaurant_frame">
-            Advertisement
+        <div className="advertisement_frame">
+            <Box className="ads_media">
+                <video 
+                loop
+                muted
+                playsInline={true}
+                autoPlay
+                >
+                    <source
+                    data-src="https://fnb-p-c6e19b1675f4.imgix.net/87994cc2-0b2c-4b4e-a402-45c9834c267e/HP-6s-1920x1080-3_def.mp4?auto=compress%2Cformat&fit=min&fm=jpg&q=80"
+                    type={"video/mp4"}
+                    src={"https://fnb-p-c6e19b1675f4.imgix.net/87994cc2-0b2c-4b4e-a402-45c9834c267e/HP-6s-1920x1080-3_def.mp4?auto=compress%2Cformat&fit=min&fm=jpg&q=80"} 
+                    />
+                </video>
+            </Box>
         </div>
     )
 }
